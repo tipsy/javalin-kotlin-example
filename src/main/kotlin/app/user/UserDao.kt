c17433902aa7c96dabda3e87831eca9a743b2314
David <davidaase@hotmail.com>
2018-10-06 18:21:59 +0200

    Bump!

--- src/main/kotlin/app/user/UserDao.kt
+++ src/main/kotlin/app/user/UserDao.kt
@@ -17,7 +17,7 @@ class UserDao {
 
     fun save(name: String, email: String) {
         val id = lastId.incrementAndGet()
-        users.put(id, User(name = name, email = email, id = id))
+        users[id] = User(name = name, email = email, id = id)
     }
 
     fun findById(id: Int): User? {
@@ -29,7 +29,7 @@ class UserDao {
     }
 
     fun update(id: Int, user: User) {
-        users.put(id, User(name = user.name, email = user.email, id = id))
+        users[id] = User(name = user.name, email = user.email, id = id)
     }
 
     fun delete(id: Int) {
